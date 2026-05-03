40903afe4ed6085d5152e48c09944db24f01af03
huang_ruimin <huang_ruimin@whu.edu.cn>
2026-05-03 20:40:18 +0800

    Modify some config

--- model/LW_DSTransNet_Student/Config.py
+++ model/LW_DSTransNet_Student/Config.py
@@ -7,11 +7,11 @@ import ml_collections
 def get_DSTransNet_Student_config():
     config = ml_collections.ConfigDict()
     config.transformer = ml_collections.ConfigDict()
-    config.KV_size = 60  # KV_size = 32+64+128+256   # 二、8+16+32+64=120    60   120
+    config.KV_size = 60  # KV_size = 32+64+128+256   # Part2、8+16+32+64=120    60   120
     config.transformer.num_heads = 4
-    config.transformer.num_layers = 1                  # 三、改成了1
+    config.transformer.num_layers = 1                  # Part3、1
     config.patch_sizes = [16, 8, 4, 2]
-    config.base_channels = 4  # base channel of U-Net     # 一、   4  8
+    config.base_channels = 4  # base channel of U-Net     # Part1、   4  8
     config.n_classes = 1
 
     # ********** unused **********
@@ -24,11 +24,11 @@ def get_DSTransNet_Student_config():
 def get_DSTransNet_Student_config_v5():
     config = ml_collections.ConfigDict()
     config.transformer = ml_collections.ConfigDict()
-    config.KV_size = 120  # KV_size = 32+64+128+256   # 二、8+16+32+64=120    60   120
+    config.KV_size = 120  # KV_size = 32+64+128+256   # Part2、8+16+32+64=120    60   120
     config.transformer.num_heads = 4
-    config.transformer.num_layers = 1                  # 三、改成了1
+    config.transformer.num_layers = 1                  # Part3、1
     config.patch_sizes = [16, 8, 4, 2]
-    config.base_channels = 8  # base channel of U-Net     # 一、   4  8
+    config.base_channels = 8  # base channel of U-Net     # Part1、   4  8
     config.n_classes = 1
 
     # ********** unused **********
@@ -41,11 +41,11 @@ def get_DSTransNet_Student_config_v5():
 def get_DSTransNet_Student_config_v34():
     config = ml_collections.ConfigDict()
     config.transformer = ml_collections.ConfigDict()
-    config.KV_size = 240  # KV_size = 32+64+128+256   # 二、8+16+32+64=120    60   120
+    config.KV_size = 240  # KV_size = 32+64+128+256   # Part2、8+16+32+64=120    60   120
     config.transformer.num_heads = 4
-    config.transformer.num_layers = 1                  # 三、改成了1
+    config.transformer.num_layers = 1                  # Part3、1
     config.patch_sizes = [16, 8, 4, 2]
-    config.base_channels = 16  # base channel of U-Net     # 一、   4  8
+    config.base_channels = 16  # base channel of U-Net     # Part1、   4  8
     config.n_classes = 1
 
     # ********** unused **********
